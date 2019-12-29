d9cd1a894931367c798adc908de0c7f8b7406d6d
Stephanie <stephaniiehallberg@gmail.com>
2019-12-29 11:04:52 +0100

    adding header with links

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from "react-dom";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/homepage/homepage.component";
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </BrowserRouter>
   );
